167fdb21c48acd0a7e0cc1f913784398b40e5656
kyungseok.lee <lks574@naver.com>
2025-08-27 14:17:34 +0900

    feat: 컬러 추가

---
+++ brainy/Sources/Core/Color/Color+Extension.swift
@@ -0,0 +1,21 @@
+import SwiftUI
+
+extension Color {
+  static let brainyAccent = Color("BrainyAccent")
+  static let brainyBackground = Color("BrainyBackground")
+  static let brainyCardBackground = Color("BrainyCardBackground")
+  static let brainyPrimary = Color("BrainyPrimary")
+  static let brainySecondary = Color("BrainySecondary")
+  static let brainySuccess = Color("BrainySuccess")
+  static let brainySurface =  Color("BrainySurface")
+  static let brainyText = Color("BrainyText")
+  static let brainyTextSecondary = Color("BrainyTextSecondary")
+
+  static let brainyError = Color.red
+  static let brainyWarning = Color.orange
+  static let brainyInfo = Color.blue
+  static let brainyCorrect = Color("BrainySuccess")
+  static let brainyIncorrect = Color.red
+  static let brainySelected = Color("BrainyPrimary")
+  static let brainyUnselected = Color("BrainyTextSecondary")
+}
